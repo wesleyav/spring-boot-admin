bec3fa3a5d9b8f7fdb3a127b20134105ccb88024
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-02 10:11:35 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/github/wesleyav/springbootadmin/SpringbootadminApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootadminApplication.class, args);
+	}
+
+}
